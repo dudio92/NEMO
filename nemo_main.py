30e97099665c169e63c86387b7f106313a5ac7e6
Ido Shraga <ido@cyabra.com>
2021-08-16 18:15:11 +0300

    debug

--- nemo_main.py
+++ nemo_main.py
@@ -14,6 +14,8 @@ class NemoHebrewNer(metaclass=Singleton):
         text_input is a textual string input, which may contain more than one sentence separated with.
         The model will analyze each sentence separately and will extract entities from all sentences.
         '''
+        print('get_hebrew_nemo_ner', text_input)
+
         vanilla_ner_model_result = run_ner_model_direct(text_input, model=self.loaded_models['token-multi'],
                                                         model_name='token-multi')
         # text_entity_tuple_list_v = list(zip(vanilla_ner_model_result['tokenized_text'][0], vanilla_ner_model_result['nemo_predictions'][0]))
@@ -23,6 +25,7 @@ class NemoHebrewNer(metaclass=Singleton):
 
         flatten_list = [item for sublist in text_entity_tuple_list for item in sublist]
         output_entities_list = []
+        print('flatten_list', flatten_list)
 
         for word_entity_tuple in flatten_list:
             # TODO: combine first + last name ini output
